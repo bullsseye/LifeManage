f5c74155019dbab728cfff59e2c35cf439bd535e
Raman Gupta <ramgupt@microsoft.com>
2019-07-18 19:42:23 +0530

    Initial Commit

---
+++ LifeManage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeManage
+//
+//  Created by Raman Gupta on 18/07/19.
+//  Copyright © 2019 Raman Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
